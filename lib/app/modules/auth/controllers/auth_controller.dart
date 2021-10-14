54dfb0a097c8144a0424810e07580cb7102f0770
vladislav <velundzv@yandex.ru>
2021-10-14 10:15:32 +0500

    refactoring

--- lib/app/modules/auth/controllers/auth_controller.dart
+++ lib/app/modules/auth/controllers/auth_controller.dart
@@ -7,64 +7,6 @@ import 'package:sample_project/app/routes/app_pages.dart';
 import 'package:sample_project/controllers/controllers.dart';
 
 class AuthController extends GetxController {
-  final email = ''.obs;
-  final password = ''.obs;
-  final signIn = false.obs;
-  void switchSign() {
-    signIn.value = !signIn.value;
-  }
-
-  void reg() async {
-    if (password.value.length < 8) {
-      return Get.showSnackbar(GetBar(
-        duration: Duration(seconds: 3),
-        messageText: Text(
-          'Пароль должен быть не меньше 8 символов',
-          style: TextStyle(color: Colors.white),
-        ),
-      ));
-    }
-
-    try {
-      await ApiController.to.register(email.value, password.value);
-      await ApiController.to.login(email.value, password.value);
-    } on DioError catch (e) {
-      if (e.response?.data is Map) {
-        Get.showSnackbar(
-          GetBar(
-            duration: Duration(seconds: 3),
-            messageText: Text(
-              e.response?.data['message'].toString() ?? '',
-              style: TextStyle(color: Colors.white),
-            ),
-          ),
-        );
-      }
-    }
-  }
-
-  void login() async {
-    try {
-      await ApiController.to.login(email.value, password.value);
-    } on DioError catch (e) {
-      if (e.response?.data is Map) {
-        Get.showSnackbar(
-          GetBar(
-            duration: Duration(seconds: 3),
-            messageText: Text(
-              e.response?.data['message'].toString() ?? '',
-              style: TextStyle(color: Colors.white),
-            ),
-          ),
-        );
-      }
-    }
-  }
-
-  void gohome() async {
-    UserController.userId ?? Get.toNamed(Routes.HOME);
-  }
-
   @override
   void onInit() {
     super.onInit();
--- lib/app/modules/auth/views/auth_view.dart
+++ lib/app/modules/auth/views/auth_view.dart
@@ -9,99 +9,10 @@ class AuthView extends GetView<AuthController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('AuthView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Obx(
-                () => Column(
-                  children: [
-                    _sign(
-                        Colors.blue[100],
-                        "SignUp",
-                        () => controller.reg(),
-                        "Зарегистрироваться",
-                        controller.signIn.value,
-                        () => controller.switchSign()),
-                    _sign(
-                        Colors.green[100],
-                        "SignIn",
-                        () => controller.login(),
-                        "Войти в аккаунт",
-                        !controller.signIn.value,
-                        () => controller.switchSign())
-                  ],
-                ),
-              )),
+        appBar: AppBar(
+          title: Text('AuthView'),
+          centerTitle: true,
         ),
-      ),
-    );
-  }
-
-  Widget _sign(color, buttontext, myFunc, title, isSignin, showSign) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
-        color: color,
-        child: Column(
-          children: [
-            InkWell(
-                onTap: !isSignin ? showSign : () {},
-                child: Column(
-                  children: [
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Divider(),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(title),
-                    ),
-                  ],
-                )),
-            Visibility(
-              visible: isSignin,
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      child: TextField(
-                        keyboardType: TextInputType.emailAddress,
-                        decoration: InputDecoration(hintText: "email"),
-                        onChanged: (value) => controller.email.value = value,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      child: TextField(
-                        decoration: InputDecoration(hintText: "password"),
-                        obscureText: true,
-                        onChanged: (value) => controller.password.value = value,
-                      ),
-                    ),
-                  ),
-                  Container(
-                    child: TextButton(
-                      child: Text(buttontext),
-                      onPressed: () {
-                        myFunc();
-                        FocusManager.instance.primaryFocus?.unfocus();
-                      },
-                    ),
-                  )
-                ],
-              ),
-            )
-          ],
-        ),
-      ),
-    );
+        body: Container());
   }
 }
